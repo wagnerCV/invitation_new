03ba52d5377f42ac21a7ed20e628ea8efc054397
Wagner_Moreira <61630676+wagnerCV@users.noreply.github.com>
2026-06-04 01:44:11 +0800

    Update HeroSection.tsx

--- client/src/components/HeroSection.tsx
+++ client/src/components/HeroSection.tsx
@@ -35,8 +35,8 @@ export default function HeroSection() {
           alt="Wedding background" 
           className="
             h-full w-full object-cover
-            object-[55%_center]
-            sm:object-[56%_center]
+            object-[58%_center]
+            sm:object-[62%_center]
             md:object-center
           "
         />
